1b509fb692e5cddcd048dd59747b188c2934ba28
dhucsik <diasgalikhanov@gmail.com>
2024-03-31 04:09:44 +0500

    commit

--- internal/transport/http/handlers/personal/router.go
+++ internal/transport/http/handlers/personal/router.go
@@ -11,5 +11,18 @@ func NewController() *Controller {
 }
 
 func (r *Controller) Init(router *echo.Group) {
+	router.GET("/dhucsik", func(c echo.Context) error {
+		return c.HTML(200, `
+			<a href="/api/v1/dhucs/love"><h1>from dhucs to ayau</h1></a>		
+		`)
+	})
+
+	router.GET("/alii", func(c echo.Context) error {
+		return c.HTML(200, `
+			<h1>ali chert</h1>		
+		`)
+
+	})
+
 	router.File("/dhucs/love", "files/index.html")
 }
