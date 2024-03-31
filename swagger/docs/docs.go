b54fde72c7ea9ab7827dde77b5f212cbe2a74336
dhucsik <diasgalikhanov@gmail.com>
2024-04-01 00:36:12 +0500

    commit

--- swagger/docs/docs.go
+++ swagger/docs/docs.go
@@ -1036,7 +1036,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "bookers.kz",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger Bookers API",
